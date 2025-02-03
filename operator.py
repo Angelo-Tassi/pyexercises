7b7f179c3b99dbd55639ec0d26c0273e50aa1325
Angelo Tassi <angelotax@gmail.com>
2025-02-03 23:20:48 +0100

    Mod

--- operator.py
+++ operator.py
@@ -1,11 +1,10 @@
-#Simple program using operator %  to check
-#if a remainder of a division is even or not.
+#Simple program using modulo operator %  to check if a number is odd or even
 
-first_number = (int(input("Number you want to check with operator :")))
-dividend_operator = (int(input("Please specify your dividend :")))
-perform_operation = first_number % dividend_operator
+first_number = (int(input("Number you want to check with modulo operator :")))
+print ("Let's check if this number can be perfectly divided by 2...")
+perform_operation = first_number % 2
 print (f"The remainder for this division is : {perform_operation}")
 if perform_operation  > 0:
-    print("The remainder is uneven.")
+    print("The number is odd.")
 else:
-    print ("The remainder is even.")
+    print ("The number is even.")
