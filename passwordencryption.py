3dc93599ac2cef9e49542d2046d511ed0aa6ec39
Angelo Tassi <angelotax@gmail.com>
2025-02-19 12:16:54 +0100

    Part 1 complete

--- passwordencryption.py
+++ passwordencryption.py
@@ -1,36 +1,33 @@
 from html.parser import charref
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-keyword= []
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
-
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
-
 def encrypt (original_text, shift_amount):
-        if direction == "encode":
-            for letter in original_text:
-                newletter=letter in alphabet.index(+ shift_amount)
+    newletter =""
+    if direction == "encode":
+        for letter in original_text:
+            #1st gets first letter's alphabet's list index in [number] format, then shifts the index by desired amount
+            shifted_letter = alphabet.index(letter) + shift_amount
 
+            #calculate possible out of range (remainder of letter / lenght of alphabet list) in order to restart from beginning
+            shifted_letter %= len(alphabet)
 
+            #adds the processed letter to the newletter string
+            newletter += alphabet[shifted_letter]
 
+        print(f"The encoded result is {newletter}")
 
-            print(f"{newletter}")
-
-
-
-
+    else:
+        print("Wrong input, please retry !")
 
 
 encrypt(text, shift)
+
+
+
+
